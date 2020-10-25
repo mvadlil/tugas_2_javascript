3498c53494a14aca0300d082992b66f46e37204e
Muhammad Fadlil <fadlilmuhammad27@gmail.com>
2020-10-25 21:25:31 +0700

    Tugas 2 Javascript
    Tugas ini untuk Niomic bootcamp

---
+++ tugas2.js
@@ -0,0 +1,8 @@
+var number = 20;
+var string = "string";
+var boolean = true;
+var nol = null;
+var arrays = [1,2,3,4,5,6,7,8,98];
+
+console.log(number + string + boolean + nol);
+console.log(arrays);
